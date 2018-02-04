6948b1350361260c2fcad8e4e0935c859d8a668d
shuyue <sw359@njit.edu>
2018-02-04 16:27:02 -0500

    FAQs content update

--- src/components/FAQs.jsx
+++ src/components/FAQs.jsx
@@ -28,15 +28,11 @@ const FAQs = () => (
 
                     <section className="context-section">
                         <h2>SERVICE QUERIES</h2>
-                        <h3><i>What are money transfers and cross-border payments?</i></h3>
+                        <h3><i>What are cross-border payments and money transfers?</i></h3>
                         <p>Cross-border payments are also known as “remittances.” It is the transfer of money from one country to another.</p>
                         <p>These payments usually occur between family members and relatives living in different countries. This can include paying utility bills, hospital bills, school/college fees or sending money for living expenses such as food and clothing. It can also include sending money to an account you have in another country.</p>
 
                         <h3><i>How long does a transfer take?</i></h3>
-                        <p>Cross-border payments are also known as “remittances.” It is the transfer of money from one country to another.</p>
-                        <p>These payments usually occur between family members and relatives living in different countries. This can include paying utility bills, hospital bills, school/college fees or sending money for living expenses such as food and clothing. It can also include sending money to an account you have in another country.</p>
-
-                        <h3><i>What are money transfers and cross-border payments?</i></h3>
                         <p>We aim to get your money to your receiver in less than 30 minutes; at most, it may take up to two hours to complete a transfer.</p>
                         <p>In some cases, for a first-time user sending money, we may take up to 24 hours to verify and validate your account details.</p>
 
@@ -47,7 +43,7 @@ const FAQs = () => (
 
                     <section>
                         <h2>GENERAL QUERIES</h2>
-                        <h3><i>GENERAL QUERIES</i></h3>
+                        <h3><i>What is OROBO?</i></h3>
                         <p>OROBO is cross-border bill payment and money remittance service that allows customers to send money or pay bills directly to Nigeria at a low, fixed transaction fee of $4.99.</p>
                         <h3><i>How does it work?</i></h3>
                         <p>OROBO is an easy-to-use mobile and web-based payment service. Follow these easy steps to get started:</p>
@@ -90,7 +86,7 @@ const FAQs = () => (
                         <p>Vendors are businesses in Nigeria that would like to expand their customer reach to consumers in the United States, allowing direct online payments straight to their business bank account at the lowest cost.</p>
 
                         <h3><i>How do I become an agent?</i></h3>
-                        <p>You can apply to become an OROBO agent by sending an application to <a href="mailto:info@OROBO.com?Subject=Hello%20again">info@orobo.com</a>.</p>
+                        <p>You can inquire about becoming an OROBO agent by sending an email to <a href="mailto:info@OROBO.com?Subject=Hello%20again">info@orobo.com</a>.</p>
                         <h3><i>How do I join as a vendor in Nigeria?</i></h3>
                         <p>Register as a vendor by completing the following forms <a href="https://orobo.com/vendors.php">https://orobo.com/vendors.php</a><br/>If you have any questions you send them to <a href="mailto:info@OROBO.com?Subject=Hello%20again">info@orobo.com</a>.</p>
 
@@ -100,26 +96,28 @@ const FAQs = () => (
 
                     <section>
                         <h2>SECURITY QUERIES</h2>
-                        <h3><i>Is it safe to send money using OROBO?</i></h3>
-                        <p>Sending and receiving money through OROBO is extremely safe. We use SSL encrypted technology on all of our platforms to ensure your details and transactions are secure.</p>
+                        <h3>Are my details safe with OROBO?</h3>
+                        <p>Sending and receiving money through OROBO is extremely safe. We are PCI DSS compliant and use SSL encrypted technology across all our platforms to ensure your financial and personal details are private and secure.  </p>
+                        <p>OROBO uses Amazon Web Service (AWS), the leading cloud computing providers, which adds an additional layer of data security. </p>
+                        <h3><i>Is it safe to send money online?</i></h3>
+                        <p>Sending money online is far safer than carrying cash on your person. Although we offer cash pick-up services, this method is far riskier than online payment options and we recommend our alternative transfer options where possible.</p>
+                        <p>All OROBO platforms employ the highest level of security available. The entire OROBO.com website, including pages viewed without logging in, is served via encrypted, secure pages.</p>
+
+                        <h3><i>What form of identification is accepted?</i></h3>
+                        <p>We accept up-to-date passports or a current driver’s license.</p>
+
+                        <h3><i>What happens if I send money and the receiver does not get it?</i></h3>
+                        <p>If your receiver does not receive funds within two days, you may contact our support team at <a href="mailto:info@OROBO.com?Subject=Hello%20again">info@orobo.com</a>.</p>
+
                         <h3><i>How to identify a scam?</i></h3>
                         <p>To avoid scams, we recommend that you ONLY send money to one of our verified vendors or to friends and family members whom you trust. NEVER send money to unknown parties.</p>
                         <p>We ask that you report anything that seems fraudulent or suspicious to <a href="mailto:abuse@OROBO.com?Subject=Hello%20again">abuse@orobo.com</a>.</p>
-                        <h3><i>Is it safe to send money online?</i></h3>
-                        <p>Sending money online is far safer than carrying cash on your person. Although we offer agent pick-up services, this method is far riskier than online payment options and we recommend our alternative transfer options where possible.</p>
-                        <h3><i>What form of identification is accepted?</i></h3>
-                        <p>We accept up-to-date passports or a current driver’s license.</p>
-                        <h3><i>What security measure do you have in place?</i></h3>
-                        <p>OROBO has taken extra precautions to ensure your details and your money are safe and secure every step of the way.</p>
-                        <p>In addition to being PCI DSS compliant, the most stringent level of security available in the payment industry, we are also certified by Trustwave.</p>
-                        <h3><i>What happens if I send money and the receiver does not get it?</i></h3>
-                        <p>If your receiver does not receive funds within two days, you may contact our support team at <a href="mailto:info@OROBO.com?Subject=Hello%20again">info@orobo.com</a>.</p>
                     </section>
 
                     <section>
                         <h2>TECHNICAL/USAGE QUERIES</h2>
                         <h3><i>Can you use this on Android and iPhone?</i></h3>
-                        <p>Yes, OROBO is available to users on both iPhone and Android devices. You can download the application from the App Store or Google Play Store. </p>
+                        <p>Yes, OROBO is available to users on both iPhone and Android devices. You can download the application from the App Store or <a href="https://play.google.com/store?hl=en">Google Play Store</a>. </p>
                         <p>Always ensure you have the latest operating system installed on your device for the optimal OROBO experience.  </p>
                         <h3><i>Why will it not allow me to select “Next”?</i></h3>
                         <p>If you are unable to select the “Next” button, please go through our checklist below before continuing. Alternatively, you can contact us via our chat or email us at <a href="mailto:info@OROBO.com?Subject=Hello%20again">info@orobo.com</a>.</p>
@@ -136,13 +134,13 @@ const FAQs = () => (
                     <section>
                         <h2>PAYMENTS /PRICING QUERIES</h2>
                         <h3><i>How much does it cost?</i></h3>
-                        <p>You can send money and pay bills at the low fixed fee of $4.99 per transaction. You can visit our homepage and enter the amount you would like to send and see what the receiver will actually receive.</p>
+                        <p>You can send money and pay bills at the low, fixed fee of $4.99 per transaction. You can visit our homepage and enter the amount you would like to send and see what the receiver will actually receive.</p>
                         <h3><i>How much can I send?</i></h3>
                         <p>Any amount between $1 and $5000 can be sent. You are charged the same fixed transaction fee of $4.99 to send any amount within that range. That means, the more you send the more you save.</p>
                         <h3><i>What exchange rate do you use?</i></h3>
                         <p>OROBO uses AbokiFx to determine the exchange rate used. You can check the rates by visiting <a href="www.abokifx.com">www.abokifx.com</a>.</p>
                         <h3><i>How much will my bank charge me?</i></h3>
-                        <p>We are not responsible for fees that may be imposed by financial institutions associated with your transaction. For example, some credit card issuers may treat the use of your credit card to use the Service as a "cash advance" and may impose additional fees and interest cost for the transaction. OROBO is not responsible for any cash advance fees, NSF fees, chargeback fees, or other, similar charges that might be imposed on you by your bank, credit card issuer, or other provider.</p>
+                        <p>We are not responsible for fees that may be imposed by financial institutions associated with your transaction. For example, some credit card issuers may treat the use of your credit card to use the Service as a "<b>cash advance</b>" and may impose additional fees and interest cost for the transaction. OROBO is not responsible for any cash advance fees, NSF fees, chargeback fees, or other, similar charges that might be imposed on you by your bank, credit card issuer, or other provider.</p>
                         <p>If you submit a transaction that results in us being charged NSF fees, chargeback fees, or other similar costs, you agree to reimburse OROBO for all such fees.</p>
                         <h3><i>What are the different payment receiving options?</i></h3>
                         <p>OROBO has five options available:</p>
@@ -157,7 +155,7 @@ const FAQs = () => (
                         <p><b>Money transfers</b> can be cancelled at any time prior to completion. Completion means that the money was sent to and received by the target banking institution, as directed by the Sender. The Transaction Amount will not be refunded after completion. All refunds will be credited to the same Payment Instrument used to pay for the Transaction. Refunds are only made in U.S. dollars and will not be adjusted to account for changes in the value of the U.S. dollar or foreign currency from the time your Transaction was submitted. </p>
                         <p>In the case of <b>Bill payments</b>, we allow a buyer protection window of 72 hours to cancel a transaction and be refunded. Once the biller has acknowledged the payment, a fulfillment window is set to allow the biller to fulfill (goods/services); this period is tentatively set to 10 days with an allowed extension of three days. After the fulfillment window, a customer can raise a dispute or confirm a fulfillment. If after seven days no dispute has been raised, the system assumes you are satisfied with the biller’s services and automatically settles the bill.</p>
                         <h3><i>Where are the cash pick-up locations?</i></h3>
-                        <p>Where are the cash pick-up locations?</p>
+                        <p>Cash can be collected at any bank within Nigeria.</p>
                         <h3><i>Will my recipients be charged to receive the money I remit?</i></h3>
                         <p>Your recipients will receive the exact amount that is shown when entering the amount you want to send (which adjusts for the fee). </p>
                         <h3><i>Can you pick up money from OROBO without an ID?</i></h3>
